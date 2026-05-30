bac4723c334d027e70f46af96a732450b3223ec1
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-30 15:07:37 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,4 @@
+// إعدادات المستودعات الأساسية
 allprojects {
     repositories {
         google()
@@ -5,20 +6,7 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
+// مسح المجلدات القديمة عند إعادة البناء
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
