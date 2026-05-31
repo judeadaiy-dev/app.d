32617a713c63e4afc0210f1b19164a79bc4a47f1
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-31 19:40:40 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,18 @@
 pluginManagement {
-    val flutterSdkPath = rootProject.projectDir.absolutePath + "/flutter" // مسار افتراضي
-    
-    // محاولة قراءة مسار فلاتر من ملف local.properties
-    val properties = java.util.Properties()
-    val localProperties = file("local.properties")
-    if (localProperties.exists()) {
-        localProperties.inputStream().use { properties.load(it) }
+    val flutterSdkPath = provider {
+        val properties = java.util.Properties()
+        val localPropertiesFile = settingsDir.resolve("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
+        }
+        val settingsSdkPath = properties.getProperty("flutter.sdk")
+        if (settingsSdkPath != null) return@provider settingsSdkPath
+        val environmentSdkPath = System.getenv("FLUTTER_ROOT")
+        if (environmentSdkPath != null) return@provider environmentSdkPath
+        throw GradleException("Flutter SDK not found. Define flutter.sdk in local.properties or FLUTTER_ROOT env variable.")
     }
-    val flutterSdk = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
-    
-    if (flutterSdk != null) {
-        includeBuild("$flutterSdk/packages/flutter_tools/gradle")
-    }
+
+    includeBuild("${flutterSdkPath.get()}/packages/flutter_tools/gradle")
 
     repositories {
         google()
@@ -22,8 +23,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    // 💡 تثبيت إصدار التجميع المستقر 8.2.1 المتوافق 100% لضمان إخراج ملف الـ APK
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
+
 include(":app")
