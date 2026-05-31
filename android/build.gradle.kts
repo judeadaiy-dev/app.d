4146f734cb5349a443661cba229cfb182d0821d5
judeadaiy-dev <judeadaiy@gmail.com>
2026-05-31 13:36:00 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,13 @@
 buildscript {
+    ext.kotlin_version = '1.9.0' // تأكدي من توافق هذا الإصدار
     repositories {
         google()
         mavenCentral()
+    }
+    dependencies {
+        // هذا السطر هو الذي كان مفقوداً!
+        classpath 'com.android.tools.build:gradle:8.1.1'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
     }
 }
 
